b4dbad328c33cde6724d4224acb183be0e52ff25
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-08-28 15:18:04 -0300

    regatados dados da api

--- react-blog-axios/src/App.jsx
+++ react-blog-axios/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
   return (
-      <div className='App'>
-        <h1>Vite + React</h1>
+    <div className='App'>
+      <Navbar />
+      <div className="container">
+        <Outlet/>
+        </div>
       </div>
   )
 }
--- react-blog-axios/src/routes/Home.jsx
+++ react-blog-axios/src/routes/Home.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+import axios from 'axios'
 
+import "./Home.css"
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+  
+  const getPosts = async () => {
+    
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const data = response.data
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  useEffect(() => {
+    getPosts()
+  },[])
+
   return (
     <div>Home</div>
   )
